875f9cfcda64ae88916e15e5f64ca93ee8bf0be5
Sumukh Agarwal <sumukhagar041@gmail.com>
2025-08-04 11:38:23 +0530

    refactor

---
+++ lib/core/services/firebase_notification.dart
@@ -0,0 +1,64 @@
+import 'dart:core';
+import 'package:dummy/core/utils/log_utility.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  final FirebaseMessaging? _firebaseMessaging;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  NotificationService(this._firebaseMessaging);
+
+  Future<void> init() async {
+    if (!kIsWeb && _firebaseMessaging != null) {
+      LogUtility.warning('Initializing Firebase Messaging');
+      await _firebaseMessaging.requestPermission(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+
+      await _initLocalNotification();
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+        LogUtility.warning(
+          'Foreground message received: ${message.notification?.title}',
+        );
+      });
+
+      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+        LogUtility.warning(
+          'Notification caused app to open: ${message.notification?.title}',
+        );
+      });
+
+      RemoteMessage? initialMessage =
+          await FirebaseMessaging.instance.getInitialMessage();
+      if (initialMessage != null) {
+        LogUtility.warning(
+          'App launched via notification: ${initialMessage.notification?.title}',
+        );
+      }
+    }
+  }
+
+  Future<void> _initLocalNotification() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<String?> getToken() async {
+    return await _firebaseMessaging?.getToken();
+  }
+
+  Stream<RemoteMessage> get onMessage => FirebaseMessaging.onMessage;
+  Stream<RemoteMessage> get onMessageOpenedApp =>
+      FirebaseMessaging.onMessageOpenedApp;
+}
